061a817ef42724372aa62dabbb2732d4b7a937d3
gc10-commits <gc10@rt2go.co>
2025-10-08 15:55:45 -0400

    chore: add GitHub issue templates

--- main.py
+++
@@ -1,7 +0,0 @@
-"""Entry point for playing Stargarden."""
-
-from stargarden import run_game
-
-
-if __name__ == "__main__":
-    run_game()
--- src/stargarden/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Stargarden: a tiny narrative space adventure."""
-
-from .game import run_game
-
-__all__ = ["run_game"]
